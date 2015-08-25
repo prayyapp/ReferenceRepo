6633f1193398b9fad5c5f1b259ed4bdba8ff2bb4
Taylor Jones <monitorjbl@gmail.com>
2015-08-25 01:10:52 +0000

    hiding inactive users
    adding user location info
    
    adding highlighting to blast borders

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@ var app = angular.module('MasterBlaster', []);
 
 app.controller('BlastList', ['$scope', '$http', 'BlastService', function($scope, $http, BlastService) {
   BlastService.list().success(function(data) {
-    $scope.allBlasts = data.records;
+    $scope.allBlasts = $scope.removeInactiveUsers(data.records);
   });
+  
+  $scope.removeInactiveUsers = function(blasts){
+    return $.grep(blasts, function(blast,idx){
+      return blast.Active == 'TRUE';
+    });
+  }
 }]);
+
+app.filter('blastRepClass', function(){
+  return function(blast) {
+    if(blast.Rep > 8){
+      return 'high-rep';
+    } else {
+      return 'normal-rep';
+    }
+  }
+});
